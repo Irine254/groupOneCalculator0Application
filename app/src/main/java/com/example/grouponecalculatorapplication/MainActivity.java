250ac6d440e35613154706629d35339043e8c6b7
AC09 <stephen.wanyingo@gmail.com>
2019-07-03 14:32:58 +0300

    First update correction

--- app/src/main/java/com/example/grouponecalculatorapplication/MainActivity.java
+++ app/src/main/java/com/example/grouponecalculatorapplication/MainActivity.java
@@ -9,24 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView myTextView;
-    private Button  division;
-    private Button  multiplication;
-    private Button  delete;
-    private Button seven;
-    private Button eight;
-    private Button nine;
-    private Button minus;
-    private Button four;
-    private Button five;
-    private Button six;
-    private Button plus;
-    private Button one;
-    private Button two;
-    private Button three;
-    private Button zero;
-    private Button decimal;
-    private Button answer;
 
 
 
@@ -37,14 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-          Button division = (Button)findViewById(R.id.bt_division_id);
-          division.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                  TextView  myTextView = (TextView)findViewById(R.id.tv_screen_id);
+
+
 
               }
-          });
 
 
 
@@ -53,5 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    }
+
+
 }
